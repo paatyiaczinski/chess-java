1ddc8a5f435172a37e048a3d290f26161cec0fb7
Patricia <patricia.iaczinski@gmail.com>
2022-09-20 02:52:42 -0300

    1 classes

---
+++ src/application/Program.java
@@ -0,0 +1,16 @@
+package application;
+
+import boardLayer.Position;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Position pos = new Position(3,2);
+		
+		System.out.println(pos);
+		
+
+	}
+
+}
